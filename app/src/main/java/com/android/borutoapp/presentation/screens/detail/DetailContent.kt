bfef87fca8467e56fb4b40362afbb53ccd2e58b5
ulugbek1060 <ulugg1060@gmail.com>
2023-01-02 02:47:09 +0500

    implemented detail screen

--- app/src/main/java/com/android/borutoapp/presentation/screens/detail/DetailContent.kt
+++ app/src/main/java/com/android/borutoapp/presentation/screens/detail/DetailContent.kt
@@ -1,23 +1,31 @@
 package com.android.borutoapp.presentation.screens.detail
 
+import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import coil.compose.AsyncImage
 import com.android.borutoapp.R
 import com.android.borutoapp.domain.model.Hero
 import com.android.borutoapp.presentation.components.InfoBox
 import com.android.borutoapp.presentation.components.OrderList
 import com.android.borutoapp.ui.theme.*
+import com.android.borutoapp.utils.Constants.BASE_URL
 
 @OptIn(ExperimentalMaterialApi::class)
 @Composable
@@ -30,14 +38,100 @@ fun DetailContent(
       bottomSheetState = rememberBottomSheetState(initialValue = BottomSheetValue.Expanded)
    )
 
+   val currentSheetFraction = bottomSheetState.currentSheetFraction
+
+   val radiusAnim by animateDpAsState(
+      targetValue = if (currentSheetFraction == 1f) EXTRA_LARGE_PADDING else 0.dp
+   )
+
    BottomSheetScaffold(
+      sheetShape = RoundedCornerShape(
+        topEnd = radiusAnim,
+        topStart = radiusAnim
+      ),
       scaffoldState = bottomSheetState,
       sheetPeekHeight = SMALL_BOTTOM_SHEET_HEIGHT,
       sheetContent = {
          selectedHero?.let { BottomSheetContent(selectedHero = it) }
       },
-      content = {}
+      content = {
+         selectedHero?.let { hero ->
+            BackgroundContent(
+               heroImage = hero.image,
+               imageFraction = currentSheetFraction,
+               onCloseClicked = {
+                  navController.popBackStack()
+               }
+            )
+         }
+      }
    )
+}
+
+private const val TAG = "DetailContent"
+
+@OptIn(ExperimentalMaterialApi::class)
+val BottomSheetScaffoldState.currentSheetFraction: Float
+   get() {
+      val fraction = bottomSheetState.progress.fraction
+      val targetValue = bottomSheetState.targetValue
+      val currentValue = bottomSheetState.currentValue
+//
+//      Log.d(TAG, "fraction: ${fraction.toString()}")
+//      Log.d(TAG, "targetValue: ${targetValue.toString()}")
+//      Log.d(TAG, "currentValue: ${currentValue.toString()}")
+
+      return when {
+         currentValue == BottomSheetValue.Collapsed && targetValue == BottomSheetValue.Collapsed -> 1f
+         currentValue == BottomSheetValue.Expanded && targetValue == BottomSheetValue.Expanded -> 0f
+         currentValue == BottomSheetValue.Collapsed && targetValue == BottomSheetValue.Expanded -> 1f - fraction
+         currentValue == BottomSheetValue.Expanded && targetValue == BottomSheetValue.Collapsed -> 0f + fraction
+         else -> fraction
+      }
+   }
+
+
+@Composable
+fun BackgroundContent(
+   heroImage: String,
+   imageFraction: Float = 1f,
+   backgroundColor: Color = MaterialTheme.colors.surface,
+   onCloseClicked: () -> Unit
+) {
+   val imageUrl = "$BASE_URL$heroImage"
+   Box(
+      modifier = Modifier
+         .fillMaxSize()
+         .background(backgroundColor)
+   ) {
+      AsyncImage(
+         modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight(fraction = imageFraction + 0.4f)
+            .align(Alignment.TopStart),
+         model = imageUrl,
+         placeholder = painterResource(R.drawable.ic_placeholder),
+         error = painterResource(R.drawable.ic_placeholder),
+         contentDescription = null,
+         contentScale = ContentScale.Crop
+      )
+      Row(
+         modifier = Modifier.fillMaxWidth(),
+         horizontalArrangement = Arrangement.End
+      ) {
+         IconButton(
+            modifier = Modifier.padding(all = SMALL_PADDING),
+            onClick = onCloseClicked
+         ) {
+            Icon(
+               modifier = Modifier.size(32.dp),
+               imageVector = Icons.Default.Close,
+               contentDescription = null,
+               tint = Color.White
+            )
+         }
+      }
+   }
 }
 
 @Composable
